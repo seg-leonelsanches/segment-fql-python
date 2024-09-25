3c5caa9eec7d986ae024fa6a5d5035bd2e6e7d35
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-09-25 13:48:03 -0700

    `__repr__` method for an AST Node.

--- segment_fql/parser/ast_node.py
+++ segment_fql/parser/ast_node.py
@@ -3,5 +3,10 @@ class ASTNode:
         self.type = node_type
         self.children = children or []
 
+    def __repr__(self):
+        if self.children:
+            return f'{self.type} ({self.children})'
+        return f'{self.type}'
+
     def is_leaf(self):
         return not self.children
